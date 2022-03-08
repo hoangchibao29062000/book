59e008008335dec4551a4db719c305d4f5593725
hoangchibao29062000 <85774706+hoangchibao29062000@users.noreply.github.com>
2022-03-08 15:00:50 +0700

    Update

--- server.js
+++ server.js
@@ -399,7 +399,7 @@ server.patch('/orders/:id', ((req, res)=> {
    }
 }))
 server.use(router)
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('Run Auth API Server')
 })
